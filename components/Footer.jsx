3ea9b936369aa1d496a5f6effd9002d305e8620e
Jatin-sh13 <72269272+Jatin-sh13@users.noreply.github.com>
2023-05-16 00:03:15 +0530

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import Image from 'next/image';
+import instaIcon from '../assets/icons/insta.png';
+import twitterIcon from '../assets/icons/twitter.png';
+import telegramIcon from '../assets/icons/telegram.png';
+import facebookIcon from '../assets/icons/facebook.png';
+
+import gpsIcon from '../assets/icons/gps.png';
+import callIcon from '../assets/icons/contact.png';
+import mailIcon from '../assets/icons/mail.png';
+
+import coingeckoImage from '../assets/images/coingecko.png';
+
+const Footer = () => {
+  return (
+    <>
+      <div
+        id="footer"
+        className="sm:flex justify-center items-center gap-10 p-5"
+      >
+        <div className="relative flex flex-col justify-center items-center sm:items-start sm:ml-40">
+          <div className="flex gap-6 mb-12">
+            <Image src={instaIcon} alt="icons" />
+            <Image src={twitterIcon} alt="icons" />
+            <Image src={telegramIcon} alt="icons" />
+            <Image src={facebookIcon} alt="icons" />
+          </div>
+
+          <p className="font-normal text-lg  sm:text-2xl leading-[34px] mb-10">
+            “The one who owns the information — <br />
+            owns the world!”
+          </p>
+
+          <div className="flex gap-5 mb-8">
+            <Image src={gpsIcon} alt="icons" className="w-4 h-5" />
+            <p className="font-bold text-base leading-5">
+              Los Angeles, Lamen St. 45, 23-000
+            </p>
+          </div>
+
+          <div className="flex gap-5 mb-8">
+            <Image src={callIcon} alt="icons" className="w-4 h-4" />
+            <p className="font-bold text-base leading-5">+1-123-456-7890</p>
+          </div>
+
+          <div className="flex gap-5 mb-8">
+            <Image src={mailIcon} alt="icons" className="w-5 h-4" />
+            <p className="font-bold text-base leading-5">coinX@crypto.com</p>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center sm:ml-40">
+          <form>
+            <p className="font-bold text-2xl sm:text-5xl leading-[60px] text-center">
+              Join our Community
+            </p>
+            <div className="flex flex-col gap-5 mt-5 ">
+              <div className="flex gap-10">
+                <label>
+                  <input
+                    name="firstName"
+                    type="text"
+                    maxLength="20"
+                    placeholder="First Name"
+                    className="w-40 bg-transparent font-normal text-xl leading-[25px]  text-white capitalize border-b-2 border-gray-400 outline-none"
+                  />
+                </label>
+                <label>
+                  <input
+                    name="lastName"
+                    type="text"
+                    maxLength="20"
+                    placeholder="Last Name"
+                    className="w-40 bg-transparent font-normal text-xl leading-[25px]  text-white capitalize border-b-2 border-gray-400 outline-none"
+                  />
+                </label>
+              </div>
+
+              <label>
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  className="w-[360px] bg-transparent font-normal text-xl leading-[25px]  text-white capitalize border-b-2 border-gray-400 outline-none"
+                />
+              </label>
+              <div className="flex justify-center sm:justify-start items-center">
+                <button
+                  type="submit"
+                  className=" bg-pinkBtn  p-4 gap-[10px] rounded-full w-[107px] h-[50px] mt-4"
+                >
+                  Sign up
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center mt-10 gap-5 border-t-2 border-gray-500 border-opacity-40">
+        <Image
+          src={coingeckoImage}
+          alt="coingecko logo"
+          className="w-40 h-10 mt-5"
+        />
+        <p className="font-bold text-base leading-5 text-center">
+          Data provided by CoinGecko
+        </p>
+      </div>
+
+      <p className="font-bold text-base leading-5 text-center my-5">
+        coinX© Copyright 2023. All rights reserved
+      </p>
+    </>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
+const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const handleNavbar = () => {
+    setNavbar(!navbar);
+  };
+
+  const handleMobileNav = () => {
+    setNavbar(false);
+  };
+
+  return (
+    <div className="fixed top-0 left-0 w-full shadow-xl flex justify-between items-center  z-40 border-b-[1px] border-solid border-gray-600 ease-in duration-300 bg-black">
+      <div className="flex justify-start items-center p-5 w-2/4 ">
+        <Link href="/" className="font-bold text-2xl leading-8">
+          coinX
+        </Link>
+      </div>
+
+      <ul className="hidden sm:flex items-start p-0 gap-10 ">
+        <li className="capitalize hover:text-pinkBtn">
+          <Link href="/#about">about us</Link>
+        </li>
+        <li className="capitalize hover:text-pinkBtn">
+          <Link href="/#services">services</Link>
+        </li>
+        <li className="capitalize hover:text-pinkBtn">
+          <Link href="/#footer">how to start</Link>
+        </li>
+      </ul>
+
+      <button className="hidden sm:flex justify-center items-center gap-[10px] rounded-[56px] bg-[#FFFFFF14] text-pinkBtn py-3 px-6 capitalize hover:bg-pinkBtn hover:text-white mr-5">
+        join us
+      </button>
+
+      <div onClick={handleNavbar} className="block sm:hidden p-4 z-10">
+        {navbar ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+      </div>
+      <div
+        className={
+          navbar
+            ? 'sm:hidden absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+            : 'sm:hidden absolute top-0 right-0 bottom-0 left-[-100%] flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+        }
+      >
+        <ul
+          className="text-sm font-bold 
+      "
+        >
+          <li className="mx-7 py-4 text-4xl hover:text-pinkBtn">
+            <Link href="/" onClick={handleMobileNav}>
+              Home
+            </Link>
+          </li>
+          <li className="mx-7 py-4 text-4xl hover:text-pinkBtn">
+            <Link href="/#about" onClick={handleMobileNav}>
+              About Us
+            </Link>
+          </li>
+          <li className="mx-7 py-4 text-4xl hover:text-pinkBtn">
+            <Link href="/#services" onClick={handleMobileNav}>
+              Services
+            </Link>
+          </li>
+          <li className="mx-7 py-4 text-4xl hover:text-pinkBtn">
+            <Link href="/#footer" onClick={handleMobileNav}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
